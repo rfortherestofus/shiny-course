0ad6e44c93f1da0e09ed98d1e027be518c1d3521
Ted Laderas <roshioshi@gmail.com>
2024-02-01 15:22:47 -0800

    update

--- basics/assignment_06/app.R
+++ basics/assignment_06/app.R
@@ -1,40 +1,27 @@
 library(shiny)
 library(fivethirtyeight)
-library(ggplot2)
-library(dplyr)
 
 data(biopics)
+
 categoricalVars <- c("country", "type_of_subject", "subject_race", "subject_sex")
 
 ui <- fluidPage(
-  
-  plotOutput("paired_plot"),
-  sliderInput(inputId = "year_slider",
-              label = "Select Year to Filter",
-              min = 1915,
-              max = 2014,
-              value = 1990)
+  selectInput(inputId = "color_select", 
+              label = "Select Categorical Variable", 
+              choices = categoricalVars),
+  plotOutput("paired_plot")
 )
 
 
 server <- function(input, output) {
-  biopics_filtered <- reactive({
-    
-    biopics %>%
-      dplyr::filter(year_release < input$year_slider)
-    
-  })
-
   
-  output$paired_plot <- renderPlot({
-    # fix this line - make sure it matches the ui line above
-    
-    ggplot(biopics_filtered()) + 
+  paired_plot2 <- renderPlot({.       #2 Mistakes here
+    ggplot(biopics) + 
       aes(x=year_release, 
           y=box_office, 
-          color= subject_sex) +
+          color= .data[[input$color_select_new]]) +   #Mistake here
       geom_point()
   })
-
 }
+
 shinyApp(ui = ui, server = server)
